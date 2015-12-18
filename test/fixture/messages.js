6827de88154f628079ec6f9f516cf00c94cbf3d6
Timo Weiß <info@timo-weiss.com>
2015-12-18 13:04:40 +0100

    add text msg fixture

--- test/fixture/messages.js
+++ test/fixture/messages.js
@@ -13,4 +13,15 @@ fixtures.locationMessagePass = {
     }
 };
 
+fixtures.textMessagePass = {
+    role: 'messenger',
+    cmd: 'newmessage',
+    message_type: 'text',
+    data: {
+        'message': 'abc123',
+        'conversation_id': '012bb2568b1842959293402b06b42170',
+        'from': '012bb2568b1842959293402b06b3681b'
+    }
+};
+
 module.exports = fixtures;
