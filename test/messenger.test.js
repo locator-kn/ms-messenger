6da7dd20ca422c13d3f4693553f64b9673c42408
Timo Weiß <info@timo-weiss.com>
2015-12-18 12:37:28 +0100

    update tests

--- test/messenger.test.js
+++ test/messenger.test.js
@@ -5,11 +5,39 @@ const proxyquire =  require('proxyquire');
 const databaseStub = require('./stubs/database.stub');
 const messages = proxyquire('../lib/messages', { './database': databaseStub });
 
-test('module.newMessage', t => {
-    //messages.newMessage({cmd: 'test', bla: 'test'}, (err, data) => {
-    //    if(err) {
-    //        return t.fail();
-    //    }
-    //    t.pass();
-    //});
+test('module.newTextMessage', t => {
+    messages.newTextMessage({
+        role: 'messenger',
+        cmd: 'newmessage',
+        message_type: 'text',
+        data: {
+            'message': 'abc123',
+            'conversation_id': '012bb2568b1842959293402b06b42170',
+            'from': '012bb2568b1842959293402b06b3681b'
+        }
+    }, (err, data) => {
+        if(err) {
+            return t.fail();
+        }
+        t.pass();
+    });
+});
+
+test('module.newLocationMessage', t => {
+    messages.newLocationMessage({
+        role: 'messenger',
+        cmd: 'newmessage',
+        message_type: 'location',
+        data: {
+            'location_id': 'abc123',
+            'conversation_id': '012bb2568b1842959293402b06b42170',
+            'from': '012bb2568b1842959293402b06b3681b'
+        }
+    }, (err, data) => {
+        if(err) {
+            return t.fail();
+        }
+
+        t.ok(data);
+    });
 });
