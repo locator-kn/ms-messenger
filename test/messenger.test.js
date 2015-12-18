82de34ed34a6efe4f8e75a55775162c8690d5e2f
Timo Weiß <info@timo-weiss.com>
2015-12-18 13:19:34 +0100

    add failing tests with wrong data

--- test/messenger.test.js
+++ test/messenger.test.js
@@ -14,9 +14,23 @@ test('messages.newTextMessage', t => {
     });
 });
 
+test('messages.newTextMessage with location message', t => {
+    messages.newTextMessage(messageFixtures.locationMessagePass, (err, data) => {
+        t.is('ValidationError', err.name);
+        t.is(void 0, data);
+    });
+});
+
 test('messages.newLocationMessage', t => {
     messages.newLocationMessage(messageFixtures.locationMessagePass, (err, data) => {
         t.is(null, err);
         t.is(data._id, '5673ee68d3f839675dc860ec');
+    });
+});
+
+test('messages.newLocationMessage with text message', t => {
+    messages.newLocationMessage(messageFixtures.textMessagePass, (err, data) => {
+        t.is('ValidationError', err.name);
+        t.is(void 0, data);
     });
 });
