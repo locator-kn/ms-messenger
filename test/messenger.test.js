51de847d1e55978aa1177bd0edc2100991395118
Timo Weiß <info@timo-weiss.com>
2015-12-18 13:05:00 +0100

    use fixtures

--- test/messenger.test.js
+++ test/messenger.test.js
@@ -3,35 +3,19 @@ import test from 'ava';
 const proxyquire =  require('proxyquire');
 
 const databaseStub = require('./stubs/database.stub');
+const messageFixtures = require('./fixture/messages');
+
 const messages = proxyquire('../lib/messages', { './database': databaseStub });
 
 test('messages.newTextMessage', t => {
-    messages.newTextMessage({
-        role: 'messenger',
-        cmd: 'newmessage',
-        message_type: 'text',
-        data: {
-            'message': 'abc123',
-            'conversation_id': '012bb2568b1842959293402b06b42170',
-            'from': '012bb2568b1842959293402b06b3681b'
-        }
-    }, (err, data) => {
+    messages.newTextMessage(messageFixtures.textMessagePass, (err, data) => {
         t.is(null, err);
         t.is(data._id, '5673ee68d3f839675dc860ec');
     });
 });
 
 test('messages.newLocationMessage', t => {
-    messages.newLocationMessage({
-        role: 'messenger',
-        cmd: 'newmessage',
-        message_type: 'location',
-        data: {
-            'location_id': 'abc123',
-            'conversation_id': '012bb2568b1842959293402b06b42170',
-            'from': '012bb2568b1842959293402b06b3681b'
-        }
-    }, (err, data) => {
+    messages.newLocationMessage(messageFixtures.locationMessagePass, (err, data) => {
         t.is(null, err);
         t.is(data._id, '5673ee68d3f839675dc860ec');
     });
