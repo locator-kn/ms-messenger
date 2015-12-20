692ee5f3446cc1446fa18384f55b74e309450d60
Timo Weiß <info@timo-weiss.com>
2015-12-20 19:44:27 +0100

    add conversation fixtures

--- test/fixture/conversations.js
+++ test/fixture/conversations.js
@@ -27,5 +27,47 @@ fixtures.twoParticipantsMissingUserId = {
     }
 };
 
+fixtures.conversationsUserId = {
+    data:{
+        user_id: '11a2ae383bf25eefde31b13860842353'
+    },
+    cmd: 'getconversationbyuser',
+    role: 'messenger'
+};
+
+fixtures.getConversationsResponse = [{
+    '_id': '11a2ae383bf25eefde31b138608424ad',
+    'create_date': '2015-07-30T19:31:45.740Z',
+    'modified_date': '2015-07-30T19:32:15.864Z',
+    'participants': [{'user_id': 'locator-app', 'last_read': 0}, {
+        'user_id': '11a2ae383bf25eefde31b13860842353',
+        'last_read': 0
+    }]
+}, {
+    '_id': '5676d94b36519ae50789705b',
+    'participants': [{
+        'user_id': '11a2ae383bf25eefde31b13860842353',
+        'last_read': 123123123
+    }, {'user_id': '11a2ae383bf25eefde31b13860842353', 'last_read': 0}]
+}, {
+    '_id': '5676d94b214c73e4078408a1',
+    'participants': [{
+        'user_id': '11a2ae383bf25eefde31b13860842353',
+        'last_read': 123123123
+    }, {'user_id': '11a2ae383bf25eefde31b13860842353', 'last_read': 0}]
+}, {
+    '_id': '5676d9bc24d8d35f0824af79',
+    'participants': [{
+        'user_id': '11a2ae383bf25eefde31b13860842353',
+        'last_read': 123123123
+    }, {'user_id': '11a2ae383bf25eefde31b13860842353', 'last_read': 0}]
+}, {
+    '_id': '5676d9bc4144f360085f0fa8',
+    'participants': [{
+        'user_id': '11a2ae383bf25eefde31b13860842353',
+        'last_read': 123123123
+    }, {'user_id': '11a2ae383bf25eefde31b13860842353', 'last_read': 0}]
+}];
+
 
 module.exports = fixtures;
