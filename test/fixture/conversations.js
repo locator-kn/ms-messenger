c70135add580aa030f1a0c15f60db6709c1b9e4f
Timo Weiß <info@timo-weiss.com>
2015-12-20 18:22:09 +0100

    add fixture with missing user_id

--- test/fixture/conversations.js
+++ test/fixture/conversations.js
@@ -15,5 +15,17 @@ fixtures.twoParticipants = {
     }
 };
 
+fixtures.twoParticipantsMissingUserId = {
+    role: 'messenger',
+    cmd: 'newconversation',
+    data: {
+        participants: [{
+            last_read: 123123123
+        }, {
+            'user_id': '11a2ae383bf25eefde31b13860842353'
+        }]
+    }
+};
+
 
 module.exports = fixtures;
