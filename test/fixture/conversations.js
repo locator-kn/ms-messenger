47580413b05c547aa0ee7856bba6353430c8a6e4
Timo Weiß <info@timo-weiss.com>
2015-12-20 18:17:24 +0100

    add fixture

---
+++ test/fixture/conversations.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fixtures = {};
+
+fixtures.twoParticipants = {
+    role: 'messenger',
+    cmd: 'newconversation',
+    data: {
+        participants: [{
+            'user_id': '11a2ae383bf25eefde31b13860842353',
+            last_read: 123123123
+        }, {
+            'user_id': '11a2ae383bf25eefde31b13860842353'
+        }]
+    }
+};
+
+
+module.exports = fixtures;
