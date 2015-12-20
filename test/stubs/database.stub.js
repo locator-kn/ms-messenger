74f42514300a5f7ff66b31448f2fbe0b15392b00
Timo Weiß <info@timo-weiss.com>
2015-12-20 18:18:08 +0100

    change behavior for conversations

--- test/stubs/database.stub.js
+++ test/stubs/database.stub.js
@@ -3,6 +3,10 @@
 const fns = {};
 
 fns.insertOne = (message, collection) => {
+    if (message.participants && message.participants.length) {
+        message._id = '5673ee68d3f839675dc860ec';
+        return Promise.resolve(message);
+    }
     return Promise.resolve({insertedId: '5673ee68d3f839675dc860ec'});
 };
 
